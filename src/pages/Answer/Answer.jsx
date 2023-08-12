15d73051f53655076635672d7e54621178fc826b
natnaelmeq <123838729+natnaelmeq@users.noreply.github.com>
2023-08-12 20:13:56 +0200

    added database url

--- src/pages/Answer/Answer.jsx
+++ src/pages/Answer/Answer.jsx
@@ -4,31 +4,61 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import Header from "../Header/Header";
 import { UserContext } from "../../context/UserContext";
 import { Button } from "react-bootstrap";
-import "./Answer.css"
+import "./Answer.css";
 
 const Answer = () => {
 	const [userData, setUserData] = useContext(UserContext);
 	const [question, setQuestion] = useState({});
 	const [form, setForm] = useState({});
-	const [submittedAnswer, setSubmittedAnswer] = useState(null);
+	const [submittedAnswer, setSubmittedAnswer] = useState([]);
 	const { id } = useParams();
-	const answerInputRef = useRef(null);
+	// const answerInputRef = useRef(null);
 	const navigate = useNavigate();
-	const handleChange = (e) => {
-		setForm({ ...form, [e.target.name]: e.target.value });
-	};
 
 	useEffect(() => {
 		if (!userData.user) navigate("/login");
 	}, [userData.user, navigate]);
-	const gettoken = localStorage.getItem("authtoken");
+
+	const handleInputChange = (e) => {
+		setForm({ ...form, [e.target.name]: e.target.value });
+	};
+
+	const handleAnswerSubmission = async (e) => {
+		e.preventDefault();
+		const yourAnswer = form.SingleAnswer;
+
+		try {
+			const response = await axios.post(`/answer`, {
+				questionid: id,
+				answer: yourAnswer,
+				userId: userData?.user?.id,
+				token: localStorage.getItem("authtoken"),
+			});
+
+			
+			setSubmittedAnswer((prevAnswers) => [
+				...prevAnswers,
+				{ answer: yourAnswer, username: userData.user.display_name },
+			]);
+
+			
+			setForm({ ...form, SingleAnswer: "" });
+
+			
+			navigate(`/question/${id}`);
+
+			
+			alert("Thank you for your answer");
+		} catch (error) {
+			console.log("Error:", error.msg);
+			alert(error.msg);
+		}
+	};
 
 	useEffect(() => {
 		const fetchQuestion = async () => {
 			try {
-				const response = await axios.get(
-					`/question/question/${id}`
-				);
+				const response = await axios.get(`/question/question/${id}`);
 				const data = response.data;
 				setQuestion(data);
 			} catch (error) {
@@ -38,40 +68,12 @@ const Answer = () => {
 		fetchQuestion();
 	}, [id]);
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			const yourAnswer = answerInputRef.current.value;
-
-			const response = await axios.post(`/answer`, {
-				questionid: id,
-				answer: yourAnswer,
-				userId: userData?.user?.id,
-				token: gettoken,
-			});
-			setSubmittedAnswer([
-				...submittedAnswer,
-				{ answer: yourAnswer, username: userData.user.display_name },
-			]);
-			
-			navigate(`/question/${id}`);
-			console.log("Response Data:", response.data);
-			answerInputRef.current.value = "";
-			alert("Thank you for your answer");
-		} catch (error) {
-			console.log("Error:", error.msg);
-			alert(error.msg);
-		}
-	};
 	useEffect(() => {
 		const fetchAnswers = async () => {
 			try {
-				const response = await axios.get(
-					`/answer/allAnswerForQ/${id}`
-				);
+				const response = await axios.get(`/answer/allAnswerForQ/${id}`);
 				const data = response.data;
 				setSubmittedAnswer(data);
-				
 			} catch (error) {
 				console.log("Error:", error);
 			}
@@ -81,89 +83,89 @@ const Answer = () => {
 
 	return (
 		<>
-			<>
-				<Header />
-				<div
-					className="container"
-					style={{
-						paddingTop: "85px",
-						paddingBottom: "30px",
-					}}
-				>
-					<div className="m-5 ">
-						{question && question.question ? (
-							<div>
-								<h2>Question</h2>
-								<h4 className="">{question.question}</h4>
-								<h4 className="fw-light">{question.question_description}</h4>
-								<h6>{question.question_id}</h6>
-							</div>
-						) : (
-							<div>Loading...</div>
-						)}
-						<hr />
-						<h2>Answers From The Community</h2>
-						<hr />
-						{submittedAnswer && submittedAnswer.length > 0 ? (
-							<div>
-							
-								{submittedAnswer.map((answer, index) => (
-									<div key={index}>
-									
+			<Header />
+			<div
+				className="container"
+				style={{
+					paddingTop: "85px",
+					paddingBottom: "30px",
+				}}
+			>
+				<div className="m-5 ">
+					{/* Display Question */}
+					{question && question.question ? (
+						<div>
+							<h2>Question</h2>
+							<h4 className="">{question.question}</h4>
+							<h4 className="fw-light">{question.question_description}</h4>
+							<h6>{question.question_id}</h6>
+						</div>
+					) : (
+						<div>Loading...</div>
+					)}
 
-										<div className=" py-3 shadow mt-4 insider  ">
-											<div className="tieuser col-md-2 col-sm-12 px-4">
-												<i className="fa-solid fa-user-tie tie "></i>
-												<p className="question_user_name mt-2  ">
-													<h3 className="lead question_user_name ps-2 ">
-														{answer.username}
-													</h3>
-												</p>
-											</div>
+					<hr />
 
-											<div className="col-md-10 col-sm-1 px-4 ">
-												<h6>{answer.answer}</h6>
-											</div>
+					{/* Display Answers */}
+					<h2>Answers From The Community</h2>
+					<hr />
+
+					{submittedAnswer && submittedAnswer.length > 0 ? (
+						<div>
+							{submittedAnswer.map((answer, index) => (
+								<div key={index}>
+									<div className=" py-3 shadow mt-4 insider  ">
+										<div className="tieuser col-md-2 col-sm-12 px-4">
+											<i className="fa-solid fa-user-tie tie "></i>
+											<p className="question_user_name mt-2  ">
+												<h3 className="lead question_user_name ps-2 ">
+													{answer.username}
+												</h3>
+											</p>
+										</div>
+										<div className="col-md-10 col-sm-1 px-4 ">
+											<h6>{answer.answer}</h6>
 										</div>
 									</div>
-								))}
-							</div>
-						) : (
-							<div>No answers submitted yet.</div>
-						)}
-						<div className="my-5 text-center">
-							<h2>Answer The Above Question</h2>
+								</div>
+							))}
 						</div>
-						<form onSubmit={handleSubmit}>
-							<textarea
-								rows={4}
-								className="form-control"
-								ref={answerInputRef}
-								type="text"
-								name="ask"
-								placeholder="Your Answer ..."
-								onChange={handleChange}
-							/>
-							<span>
-								<Button className="mt-4" variant="primary" type="submit">
-									Post Your Question
-								</Button>
-								<Link to="/">
-									<Button
-										style={{
-											backgroundColor: "rgb(231, 116, 22)",
-											border: "none",
-										}}
-										className="mt-4 mx-4"
-									>
-										Back to DashBord
-									</Button>
-								</Link>
-							</span>
-						</form>
+					) : (
+						<div>No answers submitted yet.</div>
+					)}
+
+					{/* Answer Submission Form */}
+					<div className="my-5 text-center">
+						<h2>Answer The Above Question</h2>
 					</div>
+					<form onSubmit={handleAnswerSubmission}>
+						<textarea
+							rows={4}
+							className="form-control"
+							value={form.SingleAnswer || ""}
+							name="SingleAnswer"
+							placeholder="Your Answer ..."
+							onChange={handleInputChange}
+						/>
+						<span>
+							<Button className="mt-4" variant="primary" type="submit">
+								Post Your Answer
+							</Button>
+							<Link to="/">
+								<Button
+									style={{
+										backgroundColor: "rgb(231, 116, 22)",
+										border: "none",
+									}}
+									className="mt-4 mx-4"
+								>
+									Back to Dashboard
+								</Button>
+							</Link>
+						</span>
+					</form>
 				</div>
-			</>
+			</div>
 		</>
 	);
 };
--- src/pages/SignUP/SignUP.jsx
+++ src/pages/SignUP/SignUP.jsx
@@ -1,227 +1,18 @@
-// import React, { useContext, useState } from "react";
-// import { Link, useNavigate } from "react-router-dom";
-// import { UserContext } from "../../context/UserContext";
-
-// import axios from "../../axios";
-// import Header from "../Header/Header";
-// import "./SignUP.css";
-// import Container from "react-bootstrap/Container";
-// import bg from "../../assets/Image/bg-svg-f.svg";
-// import VisibilityIcon from "@mui/icons-material/Visibility";
-// import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
-
-// import { Form, Button, Col, Row } from "react-bootstrap";
-
-// const SignUp = () => {
-// 	const [userData, setuserData] = useContext(UserContext);
-// 	const [form, setform] = useState({});
-// 	const navigate = useNavigate();
-// 	const [passwordVisible, setPasswordVisible] = useState(false);
-// 	const handleChange = (e) => {
-// 		setform({ ...form, [e.target.name]: e.target.value });
-// 	};
-
-// 	const passwordchange = () => {
-// 		setPasswordVisible(!passwordVisible);
-// 	};
-
-// 	const handleSubmit = async (e) => {
-// 		e.preventDefault();
-// 		try {
-// 			await axios.post("/users", form);
-// 			alert("Successfully signed up! Please log in with your new account.");
-// 			navigate("/login");
-// 		} catch (error) {
-// 			console.log("problem", error.response.data.msg);
-// 			alert(error.response.data.msg);
-// 		}
-// 	};
-
-// 	return (
-// 		<>
-// 			<Header />
-
-// 			<div
-// 				style={{
-// 					backgroundImage: `url(${bg})`,
-// 					backgroundSize: "cover",
-// 					backgroundRepeat: "no-repeat",
-// 					paddingTop: "180px",
-// 					paddingBottom: "100px",
-// 				}}
-// 			>
-// 				<Container>
-// 					<Row>
-// 						<div className="login_container_signUp col-sm-12 col-md shadow">
-// 							<div className=" text-center mb-4">
-// 								<h5>Join the network</h5>
-// 								<p>
-// 									Already have an account?
-// 									<Link to="/login" className="create_new_acc">
-// 										{" "}
-// 										Sign in
-// 									</Link>
-// 								</p>
-// 							</div>
-// 							<Form onSubmit={handleSubmit}>
-// 								<Form.Group controlId="email">
-// 									<Form.Control
-// 										type="Email "
-// 										className="p-3 mt-2 form-control"
-// 										name="email"
-// 										placeholder="Email address"
-// 										onChange={handleChange}
-// 										required
-// 									/>
-// 								</Form.Group>
-// 								<div className="row">
-// 									<Form.Group controlId="firstName " className="col-md">
-// 										<Form.Control
-// 											type="text"
-// 											className="p-3 mt-3 "
-// 											name="firstName"
-// 											placeholder="First name"
-// 											onChange={handleChange}
-// 											required
-// 										/>
-// 									</Form.Group>
-// 									<Form.Group controlId="lastName" className="col-sm-12 col-md">
-// 										<Form.Control
-// 											type="text "
-// 											className="p-3 mt-3"
-// 											name="lastName"
-// 											placeholder="Last name"
-// 											onChange={handleChange}
-// 											required
-// 										/>
-// 									</Form.Group>
-// 								</div>
-// 								<Form.Group controlId="userName">
-// 									<Form.Control
-// 										type="text "
-// 										className="p-3 mt-3"
-// 										name="userName"
-// 										placeholder="User name"
-// 										onChange={handleChange}
-// 										required
-// 									/>
-// 								</Form.Group>
-
-// 								<Form.Group controlId="password">
-// 									<span onClick={passwordchange}>
-// 										{" "}
-// 										<input
-// 											type={passwordVisible ? "text" : "password"}
-// 											className="p-3 mt-3 form-control"
-// 											name="password"
-// 											placeholder="Password"
-// 											onChange={handleChange}
-// 										/>{" "}
-// 										<i
-// 											style={{
-// 												position: "relative",
-// 												top: "-40px",
-// 												left: "85%",
-// 												cursor: "pointer",
-// 											}}
-// 										>
-// 											{passwordVisible ? (
-// 												<VisibilityOffIcon />
-// 											) : (
-// 												<VisibilityIcon />
-// 											)}
-// 										</i>
-// 									</span>
-// 								</Form.Group>
-
-// 								<small className="px-4  text-center py-3">
-// 									I agree to the
-// 									<Link to="" className="orange">
-// 										{" "}
-// 										privacy policy
-// 									</Link>{" "}
-// 									and
-// 									<Link to="" className="orange">
-// 										{" "}
-// 										terms of service.
-// 									</Link>
-// 								</small>
-
-// 								<Button
-// 									variant="primary"
-// 									type="submit"
-// 									className="signUp_Btn"
-// 									style={{
-// 										border: "none",
-// 									}}
-// 								>
-// 									Agree and join
-// 								</Button>
-
-// 								<Link to="/login" className="text-center create_new_acc mt-2">
-// 									Already have an account?
-// 								</Link>
-// 							</Form>
-// 						</div>
-
-// 						<Col sm={12} md={6}>
-// 							<div className="pt-5 px-4">
-// 								<small style={{ color: "#f6912b" }}>About</small>
-// 								<h1 className="mb-4">Evangadi Networks</h1>
-// 								<p style={{ lineHeight: "30px" }}>
-// 									No matter what stage of life you are in, whether you’re just
-// 									starting elementary school or being promoted to CEO of a
-// 									Fortune 500 company, you have much to offer to those who are
-// 									trying to follow in your footsteps.
-// 									<br />
-// 									<br />
-// 									Whether you are willing to share your knowledge or you are
-// 									just looking to meet mentors of your own, please start by
-// 									joining the network here.
-// 								</p>
-// 								<Button
-// 									style={{
-// 										backgroundColor: "#f6912b",
-// 										border: "none",
-// 									}}
-// 								>
-// 									HOW IT WORKS
-// 								</Button>
-// 							</div>
-// 						</Col>
-// 					</Row>
-// 				</Container>
-// 			</div>
-// 		</>
-// 	);
-// };
-
-// export default SignUp;
-
 import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 // import axios from "axios";
 import "./SignUP.css";
 import Container from "react-bootstrap/Container";
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
-// import Button from "react-bootstrap/Button";
+
 import bg from "../../assets/Image/bg-svg-f.svg";
-// import "./SignUp.css";
+
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
-// import { Form, Button, Col } from "react-bootstrap";
+
 import { UserContext } from "../../context/UserContext";
-// import Header from "./Header";
-// import { Link, useNavigate } from "react-router-dom";
-// import { UserContext } from "../../context/UserContext";
 
 import axios from "../../axios";
 import Header from "../Header/Header";
 import "./SignUP.css";
-// import Container from "react-bootstrap/Container";
-// import bg from "../../assets/Image/bg-svg-f.svg";
-// import VisibilityIcon from "@mui/icons-material/Visibility";
-// import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 import { Form, Button, Col, Row } from "react-bootstrap";
 
@@ -245,56 +36,14 @@ const SignUp = () => {
 		boxShadow: "none",
 		backgroundColor: !isFocused && value === "" ? "#fcb6b6" : "white",
 	});
-	//   const getErrorStyle = (isError) => ({
-	//     backgroundColor:isError?"red":"white",
-	//   });
+
 	const handleFocus = (setstate) => {
 		setstate(true);
 	};
 	const handleBlur = (setstate) => {
 		setstate(false);
 	};
-	//   const handleEmailFocus = () => {
-	//     setEmailFocused(true);
-	//   };
 
-	//   const handleEmailBlur = () => {
-	//     setEmailFocused(false);
-	//   };
-
-	//   const handleFirstnameFocus = () => {
-	//     setFirstnameFocused(true);
-	//   };
-
-	//   const handleFirstnameBlur = () => {
-	//     setFirstnameFocused(false);
-	//   };
-
-	//   const handleLastnameFocus = () => {
-	//     setLastnameFocused(true);
-	//   };
-
-	//   const handleLastnameBlur = () => {
-	//     setLastnameFocused(false);
-	//   };
-
-	//   const handleUsernameFocus = () => {
-	//     setUsernameFocused(true);
-	//   };
-
-	//   const handleUsernameBlur = () => {
-	//     setUsernameFocused(false);
-	//   };
-
-	//   const handlePasswordFocus = () => {
-	//     setPasswordFocused(true);
-	//   };
-
-	//   const handlePasswordBlur = () => {
-	//     setPasswordFocused(false);
-	//   };
-	console.log(userData);
-	console.log(form);
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		const requiredFields = [
@@ -451,25 +200,27 @@ const SignUp = () => {
 								Agree and join
 							</Button>
 
-							<small className="px-4  text-center py-3">
-								I agree to the {" "}
+							<span className="text-center p-3">
+								I agree to the{" "}
 								<Link to="" className="orange">
-									privacy policy {" "}
+									privacy policy{" "}
 								</Link>{" "}
-								and {" "}
+								and{" "}
 								<Link to="" className="orange">
-									terms of service. {" "}
-								</Link>
-								{" "}
-							</small>
+									terms of service.{" "}
+								</Link>{" "}
+							</span>
 						</Form>
-						{/* <Link to="/login" className="color_orange">
-						{" "}
-						Already have an account?{" "}
-					</Link> */}
+						<Link to="/login" className="color_orange">
+							{" "}
+							Already have an account?{" "}
+						</Link>
 					</div>
+					<br />
+					<br />
 					<Col sm={12} md={6}>
-						<div className="pt-5 px-4 ">
+						<br />
+						<div className="pt-5 px-4 about">
 							<small style={{ color: "#f6912b" }}>About</small>
 							<h1 className="mb-4">Evangadi Networks</h1>
 							<div style={{ lineHeight: "30px", width: "75%" }}>
@@ -483,13 +234,15 @@ const SignUp = () => {
 								looking to meet mentors of your own, please start by joining the
 								network here.
 							</div>
+							<br /> 
+
 							<Button
 								style={{
 									backgroundColor: "#f6912b",
 									border: "none",
 								}}
 							>
-								HOW IT WORKS
+								<div>HOW IT WORKS</div>
 							</Button>
 						</div>
 						          
